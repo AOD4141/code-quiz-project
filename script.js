2d741d7baebb25f27039a2644f542388a0f145e3
Adebayo <adenguk5251@gmail.com>
2024-03-16 12:38:01 +0000

    added the script.js question bank for the quiz

---
+++ script.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+    question: "Where was the Olympics in 2012 held?",
+    options: [ "China" , "UK" , "Rio De Janeiro" , "London" , "Beijing"],
+    answer: "London"
+},
+
+
+{ question: "Which continent will you find Jamaica?",
+    options: ["North America", "Africa", "Asia", "Caribbean", "Oceania"],
+    answer: "North America"
+},
+
+{question: "The capital of Australia is?",
+  options: ["Melbourne", "Sydney", "Canberra","Brisbane", "Perth"],
+    answer: "Canberra"
+},
+
+    { question: "How many countries are officially member states of NATO?",
+     options: ["30", "31", "32", "29", "33"],
+     answer: "32"
+
+    },
+
+    { question: "Which of this sport is not part of the Olympics events in recent times?",
+    options: ["Cricket", "Football", "Water polo", "Equestrian", "Boxing"],
+    answer: "Cricket"
+    }
+
+];
